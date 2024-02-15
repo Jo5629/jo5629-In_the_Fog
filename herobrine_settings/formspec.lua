64395fb9fc4c7dfe5bf79e2aa1ba65162f835f4a
Jo5629 <127150661+Jo5629@users.noreply.github.com>
2024-02-14 19:20:37 -0500

    Fix and update formspec.lua

--- herobrine_settings/formspec.lua
+++ herobrine_settings/formspec.lua
@@ -5,14 +5,26 @@ local my_gui = flow.make_gui(function(player, ctx)
         return string.upper(a) < string.upper(b)
     end)
 
-    local vbox = {name = "vbox1", h = 15, w = 12}
+    local vbox = {name = "vbox1", h = 15, w = 12, spacing = 0.5}
     for _, v in pairs(herobrine_settings.settings_list) do
-        table.insert(vbox, gui.Field{type = "field", label = v})
+        local def = herobrine_settings.settings_defs[v]
+        local default = def.value
+        if def.type == "table" then
+            default = minetest.serialize(default)
+        elseif def.type ~= "string" then
+            default = tostring(default)
+        end
+
+        table.insert(vbox, gui.Field{name = v, label = string.format("%s - %s", minetest.colorize("#BFFF00", v), def.description), default = default, h = 1.3})
     end
 
    return gui.VBox{
-        gui.Label{label = "In the Fog Settings Page:"},
-        gui.ScrollableVBox(vbox)
+        gui.Label{label = "In the Fog Settings Page:", align_h = "centre"},
+        gui.ScrollableVBox(vbox),
+        gui.Button{
+            label = "Save Settings",
+            h = 1.3,
+        }
     }
 end)
 
