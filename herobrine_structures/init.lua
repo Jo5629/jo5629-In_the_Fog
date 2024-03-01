c934f6a0ab0d0555855182d0610a4e761f23b6f8
Jo5629 <127150661+Jo5629@users.noreply.github.com>
2024-03-01 16:50:40 -0500

    Add sandstone pyramid.

--- herobrine_structures/init.lua
+++ herobrine_structures/init.lua
@@ -1,14 +1,41 @@
-local structure_modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/structures"
+local structure_modpath = minetest.get_modpath(minetest.get_current_modname()) .. "/schematics"
 
 --> Tunnel.
 minetest.register_decoration({
+    name = "herobrine_structures:tunnel",
     deco_type = "schematic",
     place_on = {"group:stone"},
     sidelen = 80,
-    fill_ratio = 0.00008,
-    flags = "place_center_x,place_center_z,force_placement,all_floors",
+    fill_ratio = 0.008,
+    flags = "place_center_x,place_center_z,force_placement",
     y_max = -30000,
-    y_min = -20,
+    y_min = -100,
     schematic = structure_modpath .. "/tunnel.mts",
     rotation = "random",
+})
+
+--> Sandstone Pyramid.
+minetest.register_decoration({
+    name = "herobrine_structures:sandstone_pyramid",
+    deco_type = "schematic",
+    place_on = {"default:river_water_source", "default:water_source", "group:sand"},
+    sidelen = 80,
+    fill_ratio = 0.008,
+    flags = "place_center_x,place_center_z,force_placement,liquid_surface",
+    y_max = -30,
+    y_min = 30,
+    schematic = structure_modpath .. "/sandstone_pyramid.mts",
+    rotation = "random",
+    biomes = {
+        "taiga_ocean",
+        "snowy_grassland_ocean",
+        "grassland_ocean",
+        "coniferous_forest_ocean",
+        "deciduous_forest_ocean",
+        "sandstone_desert_ocean",
+        "cold_desert_ocean",
+        "desert",
+        "sandstone_desert",
+        "cold_desert",
+    },
 })
